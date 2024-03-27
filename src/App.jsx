7bd6d74882ee19a71d735962eefb3fab760cf61c
Mariyamahnoor95 <mariyammahnoor@gmail.com>
2024-03-27 15:30:01 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Dashboard</p>
+    </>
+  );
+}
+
+export default App;
